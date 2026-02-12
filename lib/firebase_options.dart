c92ea4cc9409405f87f8672c7221f2b26a3dc727
AnasSalem2004 <146668937+AnasSalem2004@users.noreply.github.com>
2026-02-13 00:00:30 +0200

    Create firebase_options.dart configuration file

---
+++ lib/firebase_options.dart
@@ -0,0 +1,11 @@
+// This is the configuration for Firebase options.
+
+class FirebaseOptions {
+    static const String apiKey = 'YOUR_API_KEY';
+    static const String authDomain = 'YOUR_AUTH_DOMAIN';
+    static const String databaseURL = 'YOUR_DATABASE_URL';
+    static const String projectId = 'YOUR_PROJECT_ID';
+    static const String storageBucket = 'YOUR_STORAGE_BUCKET';
+    static const String messagingSenderId = 'YOUR_MESSAGING_SENDER_ID';
+    static const String appId = 'YOUR_APP_ID';
+}
